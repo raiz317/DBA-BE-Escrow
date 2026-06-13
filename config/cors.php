4c08d428761d5baeeccebb45e833a2cf9dead4ac
Rafid Faiz Putra <raizzzptr@gmail.com>
2026-06-13 21:18:55 +0700

    Update allowed_origins for CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://dba-fe-escrow.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
